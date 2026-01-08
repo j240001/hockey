4832bc3e6c4aa4534a17d40b69d41ed432869f61
j240001 <jdufour99@gmail.com>
2026-01-08 16:41:46 -0700

    Update tournament.js

--- tournament.js
+++ tournament.js
@@ -49,7 +49,7 @@ const Tournament = {
         const numTeams = teams.length;
         const gamesPerCycle = numTeams - 1; 
         const half = numTeams / 2;
-        const TARGET_GAMES = 82;
+        const TARGET_GAMES = 12;
         let seasonLoops = Math.ceil(TARGET_GAMES / gamesPerCycle);
         if (seasonLoops < 1) seasonLoops = 1;
 
